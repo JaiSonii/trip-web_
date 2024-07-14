0ec5501fe533e77a25e9e383fe598ad19ab427b9
Jai Soni <jaiusoni2003@gmail.com>
2024-07-14 13:25:24 +0530

    removed errors

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,8 @@
 import OtpLogin from "@/components/OtpLogin";
 function LoginPage() {
 
+  console.log(process.env.NEXT_PUBLIC_FIREBASE_API_KEY)
+
   return (
     <div className="flex flex-col items-center">
       <h1 className="font-bold text-center mb-5">
--- src/firebase/firbaseConfig.ts
+++ src/firebase/firbaseConfig.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+console.log(process.env.NEXT_PUBLIC_FIREBASE_API_KEY)
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
