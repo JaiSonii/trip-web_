4643952b82a32deadd69dd13520da80ce7781afe
Jai Soni <jaiusoni2003@gmail.com>
2024-07-30 17:22:14 +0530

    minor update

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,9 @@
-
-
 export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
-    return (
-        <div className="flex items-center relative min-h-screen p-4 inset-0 bg-black bg-opacity-0 justify-center z-50 h-full">
-          <iframe src="https://lottie.host/embed/3d45f054-61ac-4d4e-837c-2fa3006e28cc/MeZ8JDXVB5.json"></iframe>
-          
-      </div>
-    )
-  }
+  return (
+    <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-10 z-50">
+      
+        <iframe 
+          src="https://lottie.host/embed/3d45f054-61ac-4d4e-837c-2fa3006e28cc/MeZ8JDXVB5.json" 
+        ></iframe>
+    </div>
+  );}
