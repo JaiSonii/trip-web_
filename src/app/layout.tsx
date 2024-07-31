28fda67cb2d42b3d8cedf228d5fae400ef3901c9
Jai Soni <jaiusoni2003@gmail.com>
2024-07-31 20:47:09 +0530

    addition of vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import '@/app/globals.css'
 import { cn } from "@/lib/utils"
 import { Inter as FontSans } from "next/font/google"
+import { Analytics } from "@vercel/analytics/react"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -26,6 +27,7 @@ export default function RootLayout({
           fontSans.variable
         )}>
         {children}
+        <Analytics />
         </body>
     </html>
   );
